2cb678afdad45a0cf538eee1d37cbc3798a823f9
szvarych <12serzhj@gmail.com>
2019-10-22 18:00:24 +0300

    added reg form players

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
-const win = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+const winCombs = [
+  [0, 1, 2], [3, 4, 5],
+  [6, 7, 8], [0, 3, 6],
+  [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
 ];
-let Oname, Xname;
-let getFormValue = function(nameX, nameO) {
-  Xname = document.getElementById("nameX").value;
-  Oname = document.getElementById("nameO").value;
-};
-function contain(index, sym) {
-  return document.getElementsByClassName("block")[index].innerHTML == sym;
-}
+let startTemplate, startContent,
+    gameTemplate, gameContent,
+    findWinner,
+    move,
+    eventTarget;
 
-window.onload = function() {
-  let startTemplate = document.getElementById("start");
-  let startContent = startTemplate.content;
-  document.body.prepend(startContent);
-
-  let gameTemplate = document.getElementById("game");
-  let gameContent = gameTemplate.content;
+let showGameTemplate = function() {
+  gameTemplate = document.getElementById("game");
+  gameContent = gameTemplate.content;
   document.body.appendChild(gameContent);
 
-  let move = 0;
-  let eventTarget = function(event) {
+  document.getElementById("gameField").addEventListener("click", eventTarget);
+}
+
+
+const contain = function (index, sym) {
+  return document.getElementsByClassName("block")[index].innerHTML === sym;
+}
+window.onload = function() {
+
+  startTemplate = document.getElementById("start");
+  startContent = startTemplate.content;
+  document.body.prepend(startContent);
+
+  move = 0;
+  eventTarget = function(event) {
     if (event.target.innerHTML === "x") {
       return;
     } else if (event.target.innerHTML === "o") {
       return;
     }
     if (event.target.className === "block") {
-      if (move % 2 == 0) {
+      if (move % 2 === 0) {
         event.target.innerHTML = "x";
       } else {
         event.target.innerHTML = "o";
@@ -43,11 +45,10 @@ window.onload = function() {
       findWinner();
     }
   };
-  document.getElementById("game_field").addEventListener("click", eventTarget);
 };
-function findWinner() {
-  for (let i = 0; i < win.length; i++) {
-    let a = win[i];
+findWinner = function () {
+  for (let i = 0; i < winCombs.length; i++) {
+    let a = winCombs[i];
     let symX = "x";
     let symO = "o";
 
@@ -55,33 +56,13 @@ function findWinner() {
         contain(a[1], symX) &&
         contain(a[2], symX)) 
         {
-      alert("Победитель " + Xname + "\n Нажмите ОК, чтобы сыграть еще раз.");
+      alert('Победитель ' + 'playerX');
     } else if (
-      contain(a[0], symO) &&
-      contain(a[1], symO) &&
-      contain(a[2], symO)
+        contain(a[0], symO) &&
+        contain(a[1], symO) &&
+        contain(a[2], symO)
     ) {
-      alert("Победитель " + Oname + "\n Нажмите ОК, чтобы сыграть еще раз.");
+      alert('Победитель ' + 'playerO');
     }
   }
 }
-// window.addEventListener("load", function() {
-//   document
-//     .getElementById("welcomeForm")
-//     .addEventListener("submit", function(event) {
-//       event.preventDefault();
-//     });
-// });
-
-// let formDefault = function (event) {
-//     event.preventDefault();
-// }
-// let ??? = function () {
-//     document.getElementById("welcomeForm").addEventListener("submit", formDefault);
-// }
-// window.addEventListener("load", ???);
-
-// let Xname = document.getElementById("nameX").value;
-// let Oname = document.getElementById("nameO").value;
-
-console.log(Xname, Oname);
