044de496d6b190d9b2f8cbd4958c98cfd3b6aeb2
Sseryyz <12serzhj@gmail.com>
2019-10-25 15:03:14 +0300

    winMsg done

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
 const winCombs = [
-  [0, 1, 2], [3, 4, 5],
-  [6, 7, 8], [0, 3, 6],
-  [1, 4, 7], [2, 5, 8],
-  [0, 4, 8], [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
-let startTemplate, startContent,
-    gameTemplate, gameContent,
-    findWinner,
-    move,
-    eventTarget,
-    playerX, playerO;
+let startTemplate,
+  startContent,
+  gameTemplate,
+  gameContent,
+  findWinner,
+  move,
+  eventTarget,
+  playerX,
+  playerO;
 
 startTemplate = document.getElementById("start");
 startContent = startTemplate.content;
 document.body.prepend(startContent);
 
-document.getElementById("welcomeForm")
-    .addEventListener("submit", function(event) {
-        event.preventDefault();
-        playerX = document.getElementById('playerX').value;
-        playerO = document.getElementById('playerO').value;
+document
+  .getElementById("welcomeForm")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+    playerX = document.getElementById("playerX").value;
+    playerO = document.getElementById("playerO").value;
 
-        goToGame();
-    });
+    goToGame();
+  });
 
 let goToGame = function() {
   hideStartTemplate();
   showGameTemplate();
-}
-
+};
 
 let hideStartTemplate = function() {
-  document.getElementById('welcomeScreen').style.display = "none";
-}
+  document.getElementById("welcomeScreen").style.display = "none";
+};
 
 let showGameTemplate = function() {
   gameTemplate = document.getElementById("game");
@@ -40,10 +47,10 @@ let showGameTemplate = function() {
   document.body.appendChild(gameContent);
 
   document.getElementById("gameField").addEventListener("click", eventTarget);
-}
-const contain = function (index, sym) {
+};
+const contain = function(index, sym) {
   return document.getElementsByClassName("block")[index].innerHTML === sym;
-}
+};
 move = 0;
 
 eventTarget = function(event) {
@@ -62,28 +69,41 @@ eventTarget = function(event) {
     findWinner();
   }
 };
-findWinner = function () {
+findWinner = function() {
   for (let i = 0; i < winCombs.length; i++) {
     let a = winCombs[i];
     let symX = "x";
     let symO = "o";
-    if (contain(a[0], symX) && contain(a[1], symX) && contain(a[2], symX) ||
-        contain(a[0], symO) && contain(a[1], symO) && contain(a[2], symO)) {
-      showWinner();
+    if (
+      contain(a[0], symX) &&
+      contain(a[1], symX) && 
+      contain(a[2], symX)) 
+      {
+      msgWinnerX();
+    } else if (
+      contain(a[0], symO) &&
+      contain(a[1], symO) &&
+      contain(a[2], symO)
+    ) {
+      msgWinnerO();
     }
   }
-}
-let showWinner = function () {
- document.getElementById("showWinner").style.display = "block";
-}
-// let msgWinnerX = function() {
-//   let textWinX = document.createTextNode(playerX);
-//   let winMsg = document.getElementById('showWinner');
-//   document.winMsg.append(textWinX);
-// }
-// let msgWinnerO = function() {
-//   let textWinO = document.createTextNode(playerO);
-//   let winMsg = document.getElementById("showWinner");
-//   document.winMsg.append(textWinO);
-// };
-
+};
+let msgWinnerX = function() {
+  let paragraph = document.createElement("p");
+  paragraph.className = "showWinner";
+  paragraph.innerHTML =
+    "Победитель " +
+    playerX +
+    '.</br>Чтобы сыграть еще раз, нажмите </br>"Начать заново."';
+  document.body.append(paragraph);
+};
+let msgWinnerO = function() {
+  let paragraph = document.createElement("p");
+  paragraph.className = "showWinner";
+  paragraph.innerHTML =
+    "Победитель " +
+    playerO +
+    '.</br>Чтобы сыграть еще раз, нажмите </br>"Начать заново."';
+  document.body.append(paragraph);
+};
